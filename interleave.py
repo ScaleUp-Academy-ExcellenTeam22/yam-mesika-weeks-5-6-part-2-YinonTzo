656222312537ea59d49ddf1aefe7c0c4a65b1f12
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-04-07 20:20:01 +0300

    Added documentation

--- interleave.py
+++ interleave.py
@@ -1,11 +1,23 @@
-def interleave(*args):
+from typing import Iterable
+
+
+def interleave(*args: Iterable) -> list:
+    """
+    :param args: Iterables.
+    :return: List with zipped values.
+    """
     return split_zipped_list(list(zip(*args)))
 
 
-def split_zipped_list(zipped_list):
+def split_zipped_list(zipped_list: list[Iterable]) -> list:
+    """
+    Accept list of Iterables and extract all values to one list.
+    :param zipped_list: List of lists.
+    :return: One lists from all lists.
+    """
     return [item for sub_list in zipped_list for item in sub_list]
 
 
 if __name__ == '__main__':
-    print(list(interleave('abc', [1, 2, 3], ('!', '@', '#'))))
-    print(list(interleave('abc', [1, 2, 3], ('!', '@', '#'), 'yinon')))
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#'), 'yinon'))
